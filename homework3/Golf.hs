89d4b8e959ed53174179ba635378e43f52c395ef
dmac <DaraMac@users.noreply.github.com>
2020-06-15 17:51:50 +0100

    Finish homework 3, delete old attempt and test function

--- homework3/Golf.hs
+++ homework3/Golf.hs
@@ -1,5 +1,7 @@
 module Golf where
 
+import Data.List
+
 -- nasty and indexy, make better!
 skips :: [a] -> [[a]]
 skips xs = [[xs !! (i-1) | i <- [1..length xs], i `mod` n == 0] | n <- [1..length xs]]
@@ -10,18 +12,11 @@ localMaxima (x:y:z:zs) = if x < y && z < y then y:(localMaxima $ z:zs) else loca
 localMaxima _ = []
 
 
--- histogram :: [Integer] -> String
--- histogram xs = map (flip replicate '*' . count xs) [0..9] 
---     where count [] _ = 0
---           count (x:xs) n = (if x == n then 1 else 0) + count xs n
--- 
-
 -- need to left pad with spaces to make tranpose work, have numbers counting down from top
 histogram :: [Integer] -> String
-histogram xs =  unlines $ map (flip replicate '*' . count xs) [0..9] ++ [replicate 10 '=', ['0'..'9']]
+histogram xs = let hist = map (flip replicate '*' . count xs) [0..9]
+                   mx = maximum $ map length hist in
+    unlines $ (transpose $ map (pad mx) hist) ++ [replicate 10 '=', ['0'..'9']]
     where count [] _ = 0
           count (x:xs) n = (if x == n then 1 else 0) + count xs n
-
-count :: [Int] -> Int -> Int
-count [] _ = 0
-count (x:xs) n = (if x == n then 1 else 0) + count xs n
+          pad n s = replicate (n - length s) ' ' ++ s
