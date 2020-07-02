cc4a9d6b1d8a75b17bc4ac8ad534af886e01b7dd
dmac <DaraMac@users.noreply.github.com>
2020-07-02 21:19:47 +0100

    Add typeclass Expr and instance of

--- homework5/Calc.hs
+++ homework5/Calc.hs
@@ -1,3 +1,32 @@
 {-# OPTIONS_GHC -Wall #-}
 module Calc where
 import ExprT
+import Parser
+
+
+-- Exercise 1
+
+eval :: ExprT -> Integer
+eval (Lit n)   = n
+eval (Add s t) = eval s + eval t
+eval (Mul s t) = eval s * eval t
+
+
+-- Exercise 2
+
+evalString :: String -> Maybe Integer
+evalString s = case parseExp Lit Add Mul s
+               of Just a  -> Just (eval a)
+                  Nothing -> Nothing
+
+
+-- Exercise 3
+
+class Expr a where
+    lit :: Integer -> a
+    add, mul :: a -> a -> a
+
+instance Expr ExprT where
+    lit = Lit
+    add = Add
+    mul = Mul
