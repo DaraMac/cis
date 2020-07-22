06fcf3a7fbaf0c28e981147fe94981c233ba538c
dmac <DaraMac@users.noreply.github.com>
2020-07-22 16:13:15 +0100

    Do some streams

--- homework6/Fibonacci.hs
+++ homework6/Fibonacci.hs
@@ -20,3 +20,39 @@ fibs2 = 0 : 1 : zipWith (+) fibs2 (tail fibs2)
 
 
 -- Exercise 3
+
+data Stream a = Cons a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x xs) = x : streamToList xs
+
+instance Show a => Show (Stream a) where
+    show = show . take 20 . streamToList
+
+
+-- Exercise 4
+
+streamRepeat :: a -> Stream a
+streamRepeat x = Cons x $ streamRepeat x
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons x xs) = Cons (f x) $ streamMap f xs
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f x = Cons x $ streamFromSeed f (f x)
+
+
+-- Exercise 5
+
+nats :: Stream Integer
+nats = streamFromSeed succ 0
+
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Cons x xs) ys = Cons x $ interleaveStreams ys xs
+
+ruler :: Stream Integer
+ruler = foldr interleaveStreams undefined $ map streamRepeat [0..]
+
+
+-- Exercise 6
+
